be232d952c5788295c052ee225479c7c1a28aaab
Atikuzzaman Sabuj <sabuj235@gmail.com>
2023-01-27 17:21:13 +0600

    List GridView example

--- lib/main.dart
+++ lib/main.dart
@@ -1,313 +1,64 @@
 import 'package:flutter/material.dart';
-void main() {
-  runApp(MyApp());
-}
+
+void main() {runApp(MyApp());}
 
 class MyApp extends StatelessWidget {
   @override
-
   Widget build(BuildContext context) {
-    final Title = 'Horizontal and Vertical List';
-
     return MaterialApp(
-      home: Scaffold(
-        appBar: AppBar(
-          title: const Text('Horizontal and Vertical  List example'),
+        home: Scaffold(appBar: AppBar(
+          title: Text("List GridView"),
         ),
-        body:
-        Column(
-          children: [
-            Container(
-              child: Padding(
-                padding: const EdgeInsets.only(left: 5,right:5),
-                child: Column(
-                  children: [
-                    Container(
-                      height: 200,
-                      child: ListView(
-                        scrollDirection: Axis.horizontal,
-                        children: [
-                          Container(
-                            width: 148.0,
-                            color: Colors.blue,
-                            child: Column (
-                              children: const [
-                                ListTile(
-                                  leading: Icon(Icons.home),
-                                  title: Text('Home'),
-                                  ),
-                              ],
-                            ),
-                          ),
-                          const SizedBox(width: 5,),
+            body: GridView.count(
+                crossAxisCount: 3,
+                crossAxisSpacing: 4.0,
+                mainAxisSpacing: 8.0,
+                children: List.generate(choices.length, (index) {
+                  return Center(
+                    child: SelectCard(choice: choices[index]),
+                  );
+                }
+                )
+            )
+        )
+    );
+  }
+}
 
-                          Container(
-                            width: 148.0,
-                            color: Colors.green,
-                            child: Column (
-                              children: const [
-                                ListTile(
-                                  leading: Icon(Icons.camera_alt),
-                                  title: Text('Camera'),
-                                ),
-                              ],
-                            ),
-                          ),
-                          const SizedBox(width: 5,),
+class Choice {
+  const Choice({required this.title, required this.icon});
+  final String title;
+  final IconData icon;
+}
 
-                          Container(
-                            width: 148.0,
-                            color: Colors.yellow,
-                            child: Column (
-                              children: const [
-                                ListTile(
-                                  leading: Icon(Icons.phone),
-                                  title: Text('Phone'),
-                                ),
-                              ],
-                            ),
-                          ),
-                          const SizedBox(width: 5,),
+const List<Choice> choices = <Choice>[
+  Choice(title: 'Home', icon: Icons.home),
+  Choice(title: 'Contact', icon: Icons.contacts),
+  Choice(title: 'Map', icon: Icons.map),
+  Choice(title: 'Phone', icon: Icons.phone),
+  Choice(title: 'Camera', icon: Icons.camera_alt),
+  Choice(title: 'Setting', icon: Icons.settings),
+  Choice(title: 'Album', icon: Icons.photo_album),
+  Choice(title: 'WiFi', icon: Icons.wifi),
+];
 
-                          Container(
-                            width: 148.0,
-                            color: Colors.red,
-                            child: Column (
-                              children: const [
-                                ListTile(
-                                  leading: Icon(Icons.map),
-                                  title: Text('Map'),
-                                ),
-                              ],
-                            ),
-                          ),
-                          const SizedBox(width: 5,),
+class SelectCard extends StatelessWidget {
+  const SelectCard({Key? key, required this.choice}) : super(key: key);
+  final Choice choice;
 
-                          Container(
-                            width: 148.0,
-                            color: Colors.lightGreenAccent,
-                            child: Column (
-                              children: const [
-                                ListTile(
-                                  leading: Icon(Icons.book),
-                                  title: Text('Book'),
-                                ),
-                              ],
-                            ),
-                          ),
-                          const SizedBox(width: 5,),
+  @override
+  Widget build(BuildContext context) {
 
-                          Container(
-                            width: 148.0,
-                            color: Colors.greenAccent,
-                            child: Column (
-                              children: const [
-                                ListTile(
-                                  leading: Icon(Icons.signal_cellular_connected_no_internet_4_bar_sharp),
-                                  title: Text('Internet'),
-                                ),
-                              ],
-                            ),
-                          ),
-                          const SizedBox(width: 5,),
-
-                          Container(
-                            width: 148.0,
-                            color: Colors.purpleAccent,
-                            child: Column (
-                              children: const [
-                                ListTile(
-                                  leading: Icon(Icons.wifi),
-                                  title: Text('WiFi'),
-                                ),
-                              ],
-                            ),
-                          ),
-                          const SizedBox(width: 5,),
-
-                          Container(
-                            width: 148.0,
-                            color: Colors.black54,
-                            child: Column (
-                              children: const [
-                                ListTile(
-                                  leading: Icon(Icons.tv),
-                                  title: Text('TV'),
-                                ),
-                              ],
-                            ),
-                          ),
-                          const SizedBox(width: 5,),
-
-                          Container(
-                            width: 148.0,
-                            color: Colors.orange,
-                            child: Column (
-                              children: const [
-                                ListTile(
-                                  leading: Icon(Icons.settings),
-                                  title: Text('Setting'),
-                                ),
-                              ],
-                            ),
-                          ),
-                        ],
-                      ),
-                    ),
-                  ],
-                ),
-
-              ),
-            ),
-
-            SizedBox(height: 10,),
-
-            Container(
-              child: Padding(
-                padding: const EdgeInsets.only(left: 5,right:120),
-                child: Column(
-                  children: [
-                    Container(
-                      height: 500,
-                      child: ListView(
-                        scrollDirection: Axis.vertical,
-                        children: [
-                          Container(
-                            width: 148.0,
-                            color: Colors.blue,
-                            child: Column (
-                              children: const [
-                                ListTile(
-                                  leading: Icon(Icons.home),
-                                  title: Text('Home'),
-                                ),
-                              ],
-                            ),
-                          ),
-
-                          const SizedBox(height: 5,),
-
-                          Container(
-                            width: 148.0,
-                            color: Colors.green,
-                            child: Column (
-                              children: const [
-                                ListTile(
-                                  leading: Icon(Icons.camera_alt),
-                                  title: Text('Camera'),
-                                ),
-                              ],
-                            ),
-                          ),
-                          const SizedBox(height: 5,),
-                          Container(
-                            width: 148.0,
-                            color: Colors.yellow,
-                            child: Column (
-                              children: const [
-                                ListTile(
-                                  leading: Icon(Icons.phone),
-                                  title: Text('Phone'),
-                                ),
-                              ],
-                            ),
-                          ),
-                          const SizedBox(height: 5,),
-
-                          Container(
-                            width: 148.0,
-                            color: Colors.red,
-                            child: Column (
-                              children: const [
-                                ListTile(
-                                  leading: Icon(Icons.map),
-                                  title: Text('Map'),
-                                ),
-                              ],
-                            ),
-                          ),
-                          const SizedBox(height: 5,),
-
-                          Container(
-                            width: 148.0,
-                            color: Colors.lightGreenAccent,
-                            child: Column (
-                              children: const [
-                                ListTile(
-                                  leading: Icon(Icons.book),
-                                  title: Text('Book'),
-                                ),
-                              ],
-                            ),
-                          ),
-                          const SizedBox(height: 5,),
-
-                          Container(
-                            width: 148.0,
-                            color: Colors.greenAccent,
-                            child: Column (
-                              children: const [
-                                ListTile(
-                                  leading: Icon(Icons.signal_cellular_connected_no_internet_4_bar_sharp),
-                                  title: Text('Internet'),
-                                ),
-                              ],
-                            ),
-                          ),
-                          const SizedBox(height: 5,),
-
-                          Container(
-                            width: 148.0,
-                            color: Colors.purpleAccent,
-                            child: Column (
-                              children: const [
-                                ListTile(
-                                  leading: Icon(Icons.wifi),
-                                  title: Text('WiFi'),
-                                ),
-                              ],
-                            ),
-                          ),
-                          const SizedBox(height: 5,),
-
-                          Container(
-                            width: 148.0,
-                            color: Colors.black54,
-                            child: Column (
-                              children: const [
-                                ListTile(
-                                  leading: Icon(Icons.tv),
-                                  title: Text('TV'),
-                                ),
-                              ],
-                            ),
-                          ),
-                          const SizedBox(height: 5,),
-
-                          Container(
-                            width: 148.0,
-                            color: Colors.orange,
-                            child: Column (
-                              children: const [
-                                ListTile(
-                                  leading: Icon(Icons.settings),
-                                  title: Text('Setting'),
-                                ),
-                              ],
-                            ),
-                          ),
-                        ],
-                      ),
-                    ),
-                  ],
-                ),
-
-              ),
-            ),
-          ],
+    return Card(
+        color: Colors.green,
+        child: Center(child: Column(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              Expanded(child: Icon(choice.icon, size:80.0,)),
+              Text(choice.title,),
+            ]
         ),
-
-      ),
+        )
     );
   }
 }
