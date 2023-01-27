0297a752ca7161358c82424da60662fe6c9de277
Atikuzzaman Sabuj <sabuj235@gmail.com>
2023-01-27 15:43:08 +0600

    Horizontal List

---
+++ lib/main.dart
@@ -0,0 +1,93 @@
+import 'package:flutter/material.dart';
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+
+  Widget build(BuildContext context) {
+    final Title = 'Flutter Horizontal Demo List';
+
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text('Phone-Book'),
+        ),
+        body:
+        Padding(
+          padding: const EdgeInsets.only(left: 5,right:5),
+          child: Container(
+            height: 200,
+            child: ListView(
+              scrollDirection: Axis.horizontal,
+              children: <Widget>[
+                Container(
+                  width: 148.0,
+                  color: Colors.blue,
+                  child: Column (
+                    children: const <Widget>[
+                      ListTile(
+                        leading: Icon(Icons.home),
+                        title: Text('Home'),
+                        ),
+                    ],
+                  ),
+                ),
+
+                Container(
+                  width: 148.0,
+                  color: Colors.green,
+                  child: Column (
+                    children: const <Widget>[
+                      ListTile(
+                        leading: Icon(Icons.camera_alt),
+                        title: Text('Camera'),
+                      ),
+                    ],
+                  ),
+                ),
+                Container(
+                  width: 148.0,
+                  color: Colors.yellow,
+                  child: Column (
+                    children: const <Widget>[
+                      ListTile(
+                        leading: Icon(Icons.phone),
+                        title: Text('Phone'),
+                      ),
+                    ],
+                  ),
+                ),
+                Container(
+                  width: 148.0,
+                  color: Colors.red,
+                  child: Column (
+                    children: const <Widget>[
+                      ListTile(
+                        leading: Icon(Icons.map),
+                        title: Text('Map'),
+                      ),
+                    ],
+                  ),
+                ),
+                Container(
+                  width: 148.0,
+                  color: Colors.orange,
+                  child: Column (
+                    children: const <Widget>[
+                      ListTile(
+                        leading: Icon(Icons.settings),
+                        title: Text('Setting'),
+                      ),
+                    ],
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
