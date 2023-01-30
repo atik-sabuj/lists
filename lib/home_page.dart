99b32a6c2275daa19b80598f4a43d7b3e9a733a7
Atikuzzaman Sabuj <sabuj235@gmail.com>
2023-01-30 15:14:04 +0600

    Layout Builder

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -1,114 +1,59 @@
+import 'package:flutter/material.dart';
+
 class HomePage extends StatefulWidget {
   const HomePage({Key? key}) : super(key: key);
 
   @override
-  _HomePageState createState() => _HomePageState();
+  State<HomePage> createState() => _HomePageState();
 }
 
 class _HomePageState extends State<HomePage> {
-  List<String> animalNames = ['Elephant', 'Tiger', 'Kangaroo'];
-  List<String> animalFamily = ['Elephantidae', 'Panthera', 'Macropodidae'];
-  List<String> animalLifeSpan = ['60-70', '8-10', '15-20'];
-  List<String> animalWeight = ['2700-6000', '90-310', '47-66'];
-  int selectedTile = 0;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('DraggableScrollableSheet'),
+        title: Text('Layout Builder'),
       ),
       body: Container(
-        child: Stack(
-          children: [
-            animalsList(),
-            bottomDetailsSheet(),
-          ],
+        color: Colors.indigo,
+
+        /// Giving dimensions to parent Container
+        /// using MediaQuery
+        /// [container's height] = [(phone's height) / 2]
+        /// [container's width] = [phone's width]
+        height: MediaQuery.of(context).size.height * 0.5,
+        width: MediaQuery.of(context).size.width,
+
+        /// Aligning contents of this Container
+        /// to center
+        alignment: Alignment.center,
+
+        child: LayoutBuilder(
+          builder: (BuildContext ctx, BoxConstraints constraints) {
+            return Container(
+              color: Colors.green,
+
+              /// Aligning contents of this Container
+              /// to center
+              alignment: Alignment.center,
+
+              /// Using parent's constraints
+              /// to calculate child's height and width
+              height: constraints.maxHeight * 0.5,
+              width: constraints.maxWidth * 0.5,
+              child: Text(
+                'LayoutBuilder Widget',
+                style: TextStyle(
+                  fontSize: 18,
+                  fontWeight: FontWeight.bold,
+                  color: Colors.white,
+                ),
+              ),
+            );
+          },
         ),
       ),
     );
   }
-
-  Widget animalsList() {
-    return ListView(
-      children: [
-        animalListTile(0, animalNames[0]),
-        animalListTile(1, animalNames[1]),
-        animalListTile(2, animalNames[2]),
-      ],
-    );
-  }
-
-  Widget animalListTile(int index, String animalName) {
-    return Padding(
-      padding: EdgeInsets.all(8.0),
-      child: ListTile(
-        onTap: () {
-          setState(() {
-            selectedTile = index;
-          });
-        },
-        title: Text(
-          animalName,
-          style: TextStyle(
-            color: Colors.brown,
-            fontSize: 24.0,
-            fontWeight: FontWeight.w600,
-          ),
-        ),
-        tileColor: Colors.lightGreen[300],
-        selected: index == selectedTile,
-        selectedTileColor: Colors.lightGreen[600],
-      ),
-    );
-  }
-
-  Widget bottomDetailsSheet() {
-    return DraggableScrollableSheet(
-      initialChildSize: .2,
-      minChildSize: .1,
-      maxChildSize: .6,
-      builder: (BuildContext context, ScrollController scrollController) {
-        return Container(
-          color: Colors.lightGreen[100],
-          child: ListView(
-            controller: scrollController,
-            children: [
-              ListTile(
-                title: Text(
-                  "NAME",
-                ),
-                subtitle: Text(
-                  animalNames[selectedTile],
-                ),
-              ),
-              ListTile(
-                title: Text(
-                  "FAMILY",
-                ),
-                subtitle: Text(
-                  animalFamily[selectedTile],
-                ),
-              ),
-              ListTile(
-                title: Text(
-                  "LIFESPAN",
-                ),
-                subtitle: Text(
-                  animalLifeSpan[selectedTile],
-                ),
-              ),
-              ListTile(
-                title: Text(
-                  "WEIGHT",
-                ),
-                subtitle: Text(
-                  animalWeight[selectedTile],
-                ),
-              ),
-            ],
-          ),
-        );
-      },
-    );
-  }
-x
+}
